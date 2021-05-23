132c7b9a6ceef20d7aea5d68fb2179e0ae34b131
TLBail <62939298+TLBail@users.noreply.github.com>
2021-05-23 23:20:31 +0200

    ajout media preview et image preview

--- scriberEditor/src/ressources/ControllerPage2.java
+++ scriberEditor/src/ressources/ControllerPage2.java
@@ -4,14 +4,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
 import sample.GenerateurExercice;
 import sample.Main;
 
+import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ControllerPage2 implements Initializable {
+
+
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    private ImageView imageView;
+
 
     private Main main;
     private PageLoader pageLoader;
@@ -43,6 +58,21 @@ public class ControllerPage2 implements Initializable {
         FileChooser chooser = new FileChooser();
         mediafilePath = chooser.showOpenDialog(null).getAbsolutePath();
         //TODO indiquer que le media est bien charger sur l'interface  et faire une vérif que le chemin obtenue est pas null
+
+        Media media = new Media(new File(mediafilePath).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+        mediaView.setMediaPlayer(mediaPlayer);
+
+         List list =  media.getTracks();
+        for (Object objet : list
+                ) {
+            System.out.println(objet.toString());
+        }
+
+        if(list.isEmpty()){
+            System.out.println("elle est vide ");
+        }
     }
 
     @FXML
@@ -50,6 +80,9 @@ public class ControllerPage2 implements Initializable {
         FileChooser chooser = new FileChooser();
         imagefilePath = chooser.showOpenDialog(null).getAbsolutePath();
         //TODO indiquer que le media est bien charger sur l'interface  et faire une vérif que le chemin obtenue est pas null
+
+        imageView.setImage(new Image(new File(imagefilePath).toURI().toString()));
+
     }
 
     @FXML
