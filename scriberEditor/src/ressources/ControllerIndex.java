d50df385cf172d7aadec980792a4277a17ff710e
TLBail <62939298+TLBail@users.noreply.github.com>
2021-05-31 21:28:36 +0200

    ajout d'un tutoriel et des lien vers sur l'app

--- scriberEditor/src/ressources/ControllerIndex.java
+++ scriberEditor/src/ressources/ControllerIndex.java
@@ -7,6 +7,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import sample.Main;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,6 +25,30 @@ public class ControllerIndex extends SuperController implements Initializable {
     @FXML
     public void onNextPageClick(ActionEvent event){
         pageLoader.loadSubPage(PageLoader.PAG1PATH);
+    }
+
+
+    @FXML
+    public void voirLeDepot(ActionEvent event){
+        try {
+            Desktop.getDesktop().browse(new URL("https://github.com/TLBail/PtutS2Grp12").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    public void voirLeTuto(ActionEvent event){
+        try {
+            Desktop.getDesktop().browse(new URL("https://github.com/TLBail/PtutS2Grp12/wiki/Cr%C3%A9er-un-exercice").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
 
