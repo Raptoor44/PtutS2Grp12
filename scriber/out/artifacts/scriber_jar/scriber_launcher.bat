de00bb24e0bfb8d25fb3398c268db9ab9b8d47c5
TLBail <62939298+TLBail@users.noreply.github.com>
2021-06-12 20:41:04 +0200

    ok

--- scriber/out/artifacts/scriber_jar/scriber_launcher.bat
+++ scriber/out/artifacts/scriber_jar/scriber_launcher.bat
@@ -4,5 +4,5 @@ echo %~dp0
 echo "parametre : "
 echo %1
 cd %~dp0
-java --module-path "..\..\..\..\.jdk\javafx-sdk-15.0.1\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.media -jar scriber.jar %1
+java --module-path "..\..\..\..\.jdk\javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.media -jar scriber.jar %1
 pause
